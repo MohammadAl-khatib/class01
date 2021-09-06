55f8a4c28d5968f55c776256ce37ab6b838e7a50
MohammadAl-khatib <alk20489@gmail.com>
2021-09-06 17:03:25 +0300

    solved lab02

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
 class HornedBeast extends Component {
+
+constructor (props){
+ super (props);
+
+ this.state = {
+     likesCounter :0
+ }
+
+}
+
+likes = () => {
+    this.setState({
+       likesCounter: this.state.likesCounter+1
+    })
+}
+
     render() {
         return (
             <>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.url} alt ='description' title ={this.props.title} />
-                <p>{this.props.description}</p>
+                <img onClick ={this.likes} src={this.props.url} alt ='description' title ={this.props.title} />
+                <img  src={'https://icon-library.com/images/small-heart-icon/small-heart-icon-0.jpg'} alt ='heart'/>
+                <p>Likes: {this.state.likesCounter}</p>
+                <p>{this.props.description}</p>     
             </>
         )
     }
